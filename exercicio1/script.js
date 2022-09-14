589b0e2469b15fb380bb47b0bd659356e0264bdd
taisserodio <taisserodio12@gmail.com>
2022-09-13 21:02:41 -0300

    Subi os exercicios da aula lacos 13.09

--- exercicio1/script.js
+++ exercicio1/script.js
@@ -5,3 +5,19 @@ const array = [
     [20, 33, 11, 12, 19],
     [3, 3, 4, 5, 10]
 ]
+
+//for(let i=0; i<array.length; i++) {
+    for(let i in array){
+    console.log(`Jogador ${Number(i)+1}: ${array[i]}`)
+}
+                                                                                        
+/*Um analista quer conseguir ver quantos gols cada um de seus 5 atacantes marcou 
+nas últimas 5 temporadas, e quer que você construa um código que mostre isso.
+
+Crie um laco for que itere pelo array deixado no arquivo script.js, 
+e em sequência itere pelo array que existe dentro do primeiro array. 
+Você deve imprimir a temporada, e o número de gols, como no exemplo abaixo:
+
+Jogador 1: 13, 15, 25, 34, 35
+Jogador 2: 30, 48, 30, 12, 47*/
+
---
+++ exercicio2/script.js
@@ -0,0 +1,24 @@
+/*# Exercício 2
+
+Crie um programa que peça um input de número para o usuário. Com este número, o código deve imprimir a **tabuada** do número, de 1 a 10.
+
+**Utilize o for...in para resolver**
+
+Exemplo com entrada **`7`**:
+```jsx
+7
+14
+21
+28
+35
+42
+49
+56
+63
+70
+*/
+let numero = Number(prompt("Digite um numero"))
+let indice = [0,1,2,3,4,5,6,7,8,9,10]
+for(let i in indice){
+    console.log(`${numero} x ${i}= ${i*numero}`)
+}
